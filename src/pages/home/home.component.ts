25b27b626e72a405a00662b2500e149ad088b4a8
antongoncharik <ant.goncharik@gmail.com>
2024-05-21 11:21:21 +0300

    Add date picker

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -3,6 +3,17 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Subscription } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
 import { DateTime } from 'luxon';
+import {
+  FormGroup,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { CommonService, TracksServices } from '../../services';
 import { Track } from '../../entities/track';
@@ -10,18 +21,33 @@ import { Track } from '../../entities/track';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSlideToggleModule, MatCardModule],
-  providers: [CommonService, TracksServices],
+  imports: [
+    MatSlideToggleModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    JsonPipe,
+    MatButtonModule,
+  ],
+  providers: [CommonService, TracksServices, provideNativeDateAdapter()],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  active: boolean = false;
   private statusSubscription: Subscription = new Subscription();
   private tracksSubscription: Subscription = new Subscription();
+  active: boolean = false;
   tracks: Track[] = [];
   activeTracks: Track[] = [];
   inactiveTracks: Track[] = [];
+  range = new FormGroup({
+    from: new FormControl<Date | null>(new Date()),
+    to: new FormControl<Date | null>(new Date()),
+  });
+  isLoadingStatus: boolean = false;
+  isLoadingTracks: boolean = false;
 
   constructor(
     private commonService: CommonService,
@@ -29,38 +55,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.statusSubscription = this.commonService.getStatus().subscribe({
-      next: (status) => (this.active = status.enabled),
-      error: (err) => console.error(err),
-    });
-    this.tracksSubscription = this.tracksService.getTracks().subscribe({
-      next: (res) => {
-        this.tracks = res;
-        this.activeTracks = res
-          .filter((item) => {
-            return item.highPrice > 0 && item.lowPrice > 0;
-          })
-          .map((item) => {
-            const date = DateTime.fromISO(item.createdAt, { zone: 'utc' });
-            const dateInZone = date.setZone('UTC+3');
-            const createdAt = dateInZone.toFormat('yyyy-MM-dd HH:mm');
-
-            return { ...item, createdAt };
-          });
-        this.inactiveTracks = res
-          .filter((item) => {
-            return item.highPrice === 0 && item.lowPrice === 0;
-          })
-          .map((item) => {
-            const date = DateTime.fromISO(item.createdAt, { zone: 'utc' });
-            const dateInZone = date.setZone('UTC+3');
-            const createdAt = dateInZone.toFormat('yyyy-MM-dd HH:mm');
-
-            return { ...item, createdAt };
-          });
-      },
-      error: (err) => console.error(err),
-    });
+    this.getStatus();
+    this.getTracks();
   }
 
   ngOnDestroy() {
@@ -68,17 +64,102 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.tracksSubscription.unsubscribe();
   }
 
-  handleToggleStatus() {
+  getStatus() {
+    this.isLoadingStatus = true;
+
+    this.statusSubscription = this.commonService.getStatus().subscribe({
+      next: (status) => {
+        this.active = status.enabled;
+
+        this.isLoadingStatus = false;
+      },
+      error: (err) => {
+        this.isLoadingStatus = false;
+        console.error(err);
+      },
+    });
+  }
+
+  toggleStatus() {
+    this.isLoadingStatus = true;
+
     this.active = !this.active;
 
     if (this.active) {
       this.commonService.switchOn().subscribe({
-        error: (err) => console.error(err),
+        next: () => {
+          this.isLoadingStatus = false;
+        },
+        error: (err) => {
+          this.isLoadingStatus = false;
+          console.error(err);
+        },
       });
     } else {
       this.commonService.switchOff().subscribe({
-        error: (err) => console.error(err),
+        next: () => {
+          this.isLoadingStatus = false;
+        },
+        error: (err) => {
+          this.isLoadingStatus = false;
+          console.error(err);
+        },
       });
     }
+  }
+
+  getTracks() {
+    this.isLoadingTracks = true;
+
+    this.tracksSubscription = this.tracksService
+      .getTracks({
+        from: DateTime.fromJSDate(this.range.value?.from ?? new Date())
+          .startOf('day')
+          .toFormat('yyyy-MM-dd HH:mm:ss'),
+        to: DateTime.fromJSDate(this.range.value?.to ?? new Date())
+          .endOf('day')
+          .toFormat('yyyy-MM-dd HH:mm:ss'),
+      })
+      .subscribe({
+        next: (res) => {
+          this.tracks = res;
+          this.activeTracks = res
+            .filter((item) => {
+              return item.highPrice > 0 && item.lowPrice > 0;
+            })
+            .map((item) => {
+              const date = DateTime.fromISO(item.createdAt, { zone: 'utc' });
+              const dateInZone = date.setZone('UTC+3');
+              const createdAt = dateInZone.toFormat('yyyy-MM-dd HH:mm');
+
+              return { ...item, createdAt };
+            });
+          this.inactiveTracks = res
+            .filter((item) => {
+              return item.highPrice === 0 && item.lowPrice === 0;
+            })
+            .map((item) => {
+              const date = DateTime.fromISO(item.createdAt, { zone: 'utc' });
+              const dateInZone = date.setZone('UTC+3');
+              const createdAt = dateInZone.toFormat('yyyy-MM-dd HH:mm');
+
+              return { ...item, createdAt };
+            });
+
+          this.isLoadingTracks = false;
+        },
+        error: (err) => {
+          this.isLoadingTracks = false;
+          console.error(err);
+        },
+      });
+  }
+
+  handleToggleStatus() {
+    this.toggleStatus();
+  }
+
+  handleClickGetTracks() {
+    this.getTracks();
   }
 }
--- src/services/tracks/tracks.service.ts
+++ src/services/tracks/tracks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -12,22 +12,37 @@ interface TrackResponse {
   created_at: string;
 }
 
+interface GetTracksParams {
+  from: string;
+  to: string;
+}
+
 @Injectable()
 export class TracksServices {
   private apiUrl = 'https://api.crypto-knight.online';
 
   constructor(private http: HttpClient) {}
 
-  getTracks(): Observable<Track[]> {
-    return this.http.get<TrackResponse[]>(this.apiUrl + '/tracks').pipe(
-      map((tracks) => {
-        return tracks.map((track) => ({
-          symbol: track.symbol,
-          highPrice: track.high_price,
-          lowPrice: track.low_price,
-          createdAt: track.created_at,
-        }));
-      })
-    );
+  getTracks(queryParams: GetTracksParams): Observable<Track[]> {
+    let params = new HttpParams();
+    for (const key in queryParams) {
+      if (queryParams.hasOwnProperty(key)) {
+        // @ts-ignore
+        params = params.set(key, queryParams[key]);
+      }
+    }
+
+    return this.http
+      .get<TrackResponse[]>(this.apiUrl + '/tracks', { params })
+      .pipe(
+        map((tracks) => {
+          return tracks.map((track) => ({
+            symbol: track.symbol,
+            highPrice: track.high_price,
+            lowPrice: track.low_price,
+            createdAt: track.created_at,
+          }));
+        })
+      );
   }
 }
